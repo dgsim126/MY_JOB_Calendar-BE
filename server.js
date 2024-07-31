fffecc0897b1197599cee27a260907713cb663a7
Shihyun Han <devowl99@gmail.com>
2024-07-31 19:30:50 +0900

    for local

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ const app = express();
 const port = 8080;
 
 app.use(cors({
-    origin: 'https://myjobcalendar.duckdns.org', // 해당 도메인만 허용
+    //origin: 'https://myjobcalendar.duckdns.org', // 해당 도메인만 허용
+    origin: true,
     credentials: true // 쿠키를 포함한 요청을 허용
 }));
 
