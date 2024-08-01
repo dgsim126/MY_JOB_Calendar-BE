b9533e72e8133dac387a267b8957427d8101ff93
dgsim <dgsim126@gmail.com>
2024-08-02 05:38:28 +0900

    create show popular

--- routers/MainCalender/MainCalenderRoute.js
+++ routers/MainCalender/MainCalenderRoute.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { optionalVerifyToken } = require('../../middleware/token');
-const { showAll } = require('../../controllers/MainCalender/MainCalenderController');
+const { showAll, showPopular } = require('../../controllers/MainCalender/MainCalenderController');
 
 // 모든 게시글 가져오기
 router.get('/', optionalVerifyToken, asyncHandler(showAll));
+
+// 인기 상위 9개 보내기
+router.get('/show', asyncHandler(showPopular));
 
 module.exports = router;
