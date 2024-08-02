97ce101eed9d9ccb999bcc7fc17595422c35545f
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-08-03 04:23:04 +0900

    model change

--- models/FreeBoard/freeboard.js
+++ models/FreeBoard/freeboard.js
@@ -18,7 +18,8 @@ class Freeboard extends Sequelize.Model {
                 allowNull: false
             },
             body: {
-                type: DataTypes.STRING,
+                // type: DataTypes.STRING,
+                type: DataTypes.TEXT,
                 allowNull: false
             },
             date: {
